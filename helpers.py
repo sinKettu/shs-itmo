be162a59c5a6efabd9020816f290e706d98cc644
sinfox <avangard.jazz@gmail.com>
2020-10-12 23:29:25 +0300

    Helpers: old helpers removed

--- helpers.py
+++ helpers.py
@@ -1,33 +1,3 @@
-import os
-
-
-def check_path(abs_path):
-    """
-        Checking URL to avoid LFI
-    """
-
-    return (not os.path.exists(abs_path) or
-            not abs_path.startswith(os.path.abspath(os.getcwd())) or
-            any([abs_path.startswith(i) for i in deprecated]) or
-            os.path.isdir(abs_path))
-
-
-def bot_slack_alert(path, host, port, tm):
-    """
-        Alert to slack via bot.
-        Requires bot's token provided by
-        SLACK_TOKEN environment variable.
-    """
-
-    if path not in alert_list:
-        return
-    tkn = os.getenv("SLACK_TOKEN")
-    mes = f"Peer `{host}` accessed endpoint `{path}` at {asctime()}."
-    url = f"https://slack.com/api/chat.postMessage?token={tkn}&" + \
-          f"channel=@aivanov&text={mes}&username=secrets_tracker&" + \
-          f"as_user=secrets_tracker&pretty=1"
-
-    ses = requests.Session()
-    response = ses.get(url)
-    code = response.status_code
-    log(get_time_record(), f"{host}:{port} alerted ({code})\n", LINE_SEPARATOR)
+import requests
+# Alerts to slack via bot
+# Alerts to slack via web-hook
