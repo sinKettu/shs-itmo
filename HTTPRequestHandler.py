ad81cdd36345b176654483510964c35762c344db
sinfox <avangard.jazz@gmail.com>
2020-10-13 23:57:00 +0300

    HRH: inheritance and other fixes

--- HTTPRequestHandler.py
+++ HTTPRequestHandler.py
@@ -1,24 +1,31 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
+
 from urls import URLHandler
+
+from mappings import default
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self, request, client_address, server):
         self.uhandler = URLHandler()
+        self.uhandler.update_urls_info()
+        self.uhandler.map_url_with_handler("/", default.handle_default)
+        super().__init__(request, client_address, server)
 
     def add_headers(self, headers):
-        for header, content in headers:
+        self.send_header("Server", "None")
+        for header in headers:
+            content = headers[header]
             self.send_header(header, content)
-        super().end_headers(self)
+        super().end_headers()
 
     def do_GET(self):
         path_content = self.path.split("?", 1)
         url = path_content[0]
         data = parse_qs(path_content[1]) if len(path_content) == 2 else {}
         method = "GET"
-        in_headers = self.headers
+        in_headers = dict(self.headers)
 
         status, data, headers = self.uhandler.handle(
             url,
