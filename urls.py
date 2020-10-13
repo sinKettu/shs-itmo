e0372c1df0a22904374c0e5531474fe05c6ded1b
sinfox <avangard.jazz@gmail.com>
2020-10-13 23:58:11 +0300

    URLs: general fixes in whole class URLHandler

--- urls.py
+++ urls.py
@@ -9,7 +9,7 @@ class URLHandler:
     static_page = 2
 
     def __init__(self):
-        self.urls_path = "config/urls"
+        self.urls_path = "config/urls.yaml"
         self.urls = {}
 
     def __parse_urls(self):
@@ -18,23 +18,24 @@ class URLHandler:
             with checks for parameters
         """
         fin = open(self.urls_path, "r")
-        urls = yaml.load_all(fin)
+        urls = fin.read()
         fin.close()
+        urls = yaml.load(urls, Loader=yaml.FullLoader)
 
         for url in urls:
             tp = urls[url].get("type", None)
             if tp is None:
                 raise NotImplementedError("Parsing Error: wrong type")
             elif tp == "none":
-                urls[url]["type"] = not_existing_page
+                urls[url]["type"] = self.not_existing_page
             elif tp == "dynamic":
                 hlr = urls[url].get("handler", 0)
-                urls[url]["type"] = dynamic_page
+                urls[url]["type"] = self.dynamic_page
                 if hlr is not None:
                     err = "Parsing Error: wrong dynamic handler"
                     raise NotImplementedError(err)
             elif tp == "static":
-                urls[url]["type"] = static_page
+                urls[url]["type"] = self.static_page
                 hlr = urls[url].get("handler", None)
                 if hlr is None:
                     err = "Parsing Error: static handler missed"
@@ -49,11 +50,11 @@ class URLHandler:
 
             methods = urls[url].get("methods", None)
             if methods is None:
-                urls[urls]["methods"] = {"GET"}
+                urls[url]["methods"] = {"GET"}
             else:
-                urls[urls]["methods"] = {i.upper() for i in methods}
+                urls[url]["methods"] = {i.upper() for i in methods}
 
-            return urls
+        return urls
 
     def update_urls_info(self):
         self.urls = self.__parse_urls()
@@ -64,7 +65,7 @@ class URLHandler:
         """
         if url not in self.urls:
             raise NotImplementedError(f"Cannot find url '{url}'")
-        elif self.urls[url]["type"] != dynamic_page:
+        elif self.urls[url]["type"] != self.dynamic_page:
             raise NotImplementedError(f"Cannot map not dynamic url '{url}'")
         elif self.urls[url]["handler"] is not None:
             raise NotImplementedError(f"Handler already exists")
@@ -77,18 +78,18 @@ class URLHandler:
         """
         if url not in self.urls:
             return 404, b"Page Not Found", []
-        elif method not in self.urls[url]:
+        elif method not in self.urls[url]["methods"]:
             return 405, b"Method not Allowed", []
-        elif self.urls[url]["type"] == not_existing_page:
+        elif self.urls[url]["type"] == self.not_existing_page:
             headers = self.urls[url].get("headers", [])
             return 404, b"Page Not Found", headers
-        elif self.urls[url]["type"] == static_page:
+        elif self.urls[url]["type"] == self.static_page:
             headers = self.urls[url].get("headers", [])
             fin = open(self.urls[url]["handler"], "rb")
             data = fin.read()
             fin.close()
             return 200, data, headers
-        elif self.urls[url]["type"] == dynamic_page:
+        elif self.urls[url]["type"] == self.dynamic_page:
             headers = self.urls[url].get("headers", [])
             status, data, headers = self.urls[url]["handler"](
                 method,
